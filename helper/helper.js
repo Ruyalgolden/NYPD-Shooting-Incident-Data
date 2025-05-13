1726ee2966ad7296031662f9480416dde39207e0
Andrew Li <161104318+Ruyalgolden@users.noreply.github.com>
2025-05-12 21:38:41 -0400

    Update helper.js

--- helper/helper.js
+++ helper/helper.js
@@ -106,15 +106,6 @@ function showMap(location){
   }).addTo(map);
 
   let marker = L.marker(location).addTo(map);
-
-  
-  let map2 = L.map("frontMap").setView(location, 14);
-  const tiles2 = L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
-    maxZoom: 18,
-    attribution: "&copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a>"
-  }).addTo(map2);
-
-  let marker2 = L.marker(location).addTo(map2);
 } 
 
 
